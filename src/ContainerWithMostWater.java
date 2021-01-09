79631d16a530922d21b194761a989c844e23f57a
Brayam Corral Flores <brayamcorral@gmail.com>
2021-01-08 16:24:59 -0800

    Test Casses Passed

--- src/ContainerWithMostWater.java
+++ src/ContainerWithMostWater.java
@@ -1,12 +1,44 @@
+/**
+ * Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai).
+ * n vertical lines are drawn such that the two endpoints of the line i is at (i, ai) and (i, 0). 
+ * Find two lines, which, together with the x-axis forms a container, such that the container contains the most water.
+ * 
+ * https://leetcode.com/problems/container-with-most-water/
+ * 
+ */
+
 public class ContainerWithMostWater {
     
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println(maxArea(new int[] {1,8,6,2,5,4,8,3,7}));
     }
 
-    public int maxArea(int[] height) {
-        
+    // Time: O(n) Space: O(1)
+    // Two pointers, move pointer that is smaller than the other pointer
+    public static int maxArea(int[] height) {
+          
+        int area = 0;
+        int p = 0;
+        int q = height.length - 1;
+        int heightBox, baseBox, newArea;
 
-        return -1;
+        while(p < q){
+
+            heightBox = (height[p] > height[q]) ? height[q] : height[p];
+            baseBox = q - p;
+            newArea = baseBox * heightBox; 
+
+            if(height[p] > height[q]){
+                q--;
+            }else{
+                p++;
+            }
+
+            if(newArea > area){
+                area = newArea;
+            }
+        }
+
+        return area;
     }
 }
